fb62caa51b50391548a734e8677bab96a70d746d
Francesco <francesco.cognolato@gmail.com>
2020-10-27 23:23:48 +1000

    Ready for generation

--- create_dataset.py
+++ create_dataset.py
@@ -5,32 +5,35 @@ import glob
 import numpy as np
 
 
+# path where to save the generated image
+result_dir = '/scratch/cai/francesco/lab2im/dataset_QSM/'
+
+# Parameters
 AUGMENTATIONS_PER_PATIENT = 100
 
-# Find filenames
-folders = glob.glob("/mnt/ResearchFS/francesco/data/extracted/*")
-folders.sort()
+for mode in ["train", "val"]:
 
-# path where to save the generated image
-resulr_dir = './dataset_QSM'
+	# Find filenames
+	folders = glob.glob("/scratch/cai/francesco/lab2im/original_data/" + mode + "/*")
+	folders.sort()
 
-for filename in folders:
-    patient_name = filename.split("/")[-1]
+	for filename in folders:
+	    patient_name = filename.split("/")[-1]
 
-    for i in range(AUGMENTATIONS_PER_PATIENT):
-        brain_generator = ImageGenerator(labels_dir=filename + "/mri/wmparc.nii",
-                                         blur_background=False)
-        im, lab = brain_generator.generate_image()
-        
-        # Mask background
-        im[lab==0] = 0.
+	    for i in range(AUGMENTATIONS_PER_PATIENT):
+	        brain_generator = ImageGenerator(labels_dir=filename + "/mri/wmparc.nii",
+	                                         blur_background=False)
+	        im, lab = brain_generator.generate_image()
+	        
+	        # Mask background
+	        im[lab==0] = 0.
 
-        # Subtract mean without background
-        im[im!=0] -= im[im!=0].mean()
+	        # Subtract mean without background
+	        im[im!=0] -= im[im!=0].mean()
 
-        # Divide by STD
-        im = im / float(round(np.random.uniform(16., 25.), 4) * im.std())
-        im = np.float32(im)
+	        # Divide by STD
+	        im = im / float(round(np.random.uniform(16., 25.), 4) * im.std())
+	        im = np.float32(im)
 
-        utils.save_volume(im, brain_generator.aff, brain_generator.header, os.path.join(resulr_dir, patient_name + "_" + str(i) + '_brain.nii.gz'))
-        utils.save_volume(lab, brain_generator.aff, brain_generator.header, os.path.join(resulr_dir, patient_name + "_" + str(i) + '_seg.nii.gz'))
+	        utils.save_volume(im, brain_generator.aff, brain_generator.header, result_dir + mode + "/" + patient_name + "_" + str(i) + '_brain.nii.gz'))
+	        utils.save_volume(lab, brain_generator.aff, brain_generator.header, result_dir + mode + "/" + patient_name + "_" + str(i) + '_seg.nii.gz'))
