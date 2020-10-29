646c35f4e05033f81dcb20f6364e765ce06cfc28
Francesco <francesco.cognolato@gmail.com>
2020-10-29 22:25:23 +1000

    Fixed

--- create_dataset.py
+++ create_dataset.py
@@ -3,41 +3,41 @@ from lab2im import utils
 from lab2im.image_generator import ImageGenerator
 import glob
 import numpy as np
+import argparse
 
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--result_dir", type=str, required=True)
+parser.add_argument("--filename", type=str, required=True)
+args = parser.parse_args()
 
 # path where to save the generated image
-result_dir = '/scratch/cai/francesco/lab2im/dataset_QSM/'
+result_dir = args.result_dir
 
 # Parameters
 AUGMENTATIONS_PER_PATIENT = 100
 
-for mode in ["train", "val"]:
+filename = args.filename
+patient_name = filename.split("/")[-1]
 
-	# Find filenames
-	folders = glob.glob("/scratch/cai/francesco/lab2im/original_data/" + mode + "/*")
-	folders.sort()
+for i in range(AUGMENTATIONS_PER_PATIENT):
+    brain_generator = ImageGenerator(labels_dir=filename + "/mri/wmparc.nii",
+                                     blur_background=False)
+    im, lab = brain_generator.generate_image()
+    
+    # Mask background
+    im[lab==0] = 0.
 
-	for filename in folders:
-	    patient_name = filename.split("/")[-1]
+    # Subtract mean without background
+    im[im!=0] -= im[im!=0].mean()
 
-	    for i in range(AUGMENTATIONS_PER_PATIENT):
-	        brain_generator = ImageGenerator(labels_dir=filename + "/mri/wmparc.nii",
-	                                         blur_background=False)
-	        im, lab = brain_generator.generate_image()
-	        
-	        # Mask background
-	        im[lab==0] = 0.
+    # Divide by STD, the smaller what inside the uniform, the broader the distribution is
+    im = im / float(round(np.random.uniform(10., 60.), 4) * im.std())
+    im = np.float32(im)
 
-	        # Subtract mean without background
-	        im[im!=0] -= im[im!=0].mean()
-
-	        # Divide by STD, the smaller what inside the uniform, the broader the distribution is
-	        im = im / float(round(np.random.uniform(10., 60.), 4) * im.std())
-	        im = np.float32(im)
-
-	        utils.save_volume(im, brain_generator.aff, brain_generator.header, result_dir + mode + "/" + patient_name + "_" + str(i) + '_brain.nii.gz')
-	        utils.save_volume(lab, brain_generator.aff, brain_generator.header, result_dir + mode + "/" + patient_name + "_" + str(i) + '_seg.nii.gz')
-	        
-	        del im
-	        del lab
-	        del brain_generator
+    utils.save_volume(im, brain_generator.aff, brain_generator.header, result_dir + patient_name + "_" + str(i) + '_brain.nii.gz')
+    utils.save_volume(lab, brain_generator.aff, brain_generator.header, result_dir + patient_name + "_" + str(i) + '_seg.nii.gz')
+    
+    del im
+    del lab
+    del brain_generator
--- slurm.sh
+++ slurm.sh
@@ -16,5 +16,13 @@ conda env list
 #conda install -y -q --name lab2im_env -c conda-forge --file requirements.txt
 source activate lab2im_env
 
-srun -n 1 python3 create_dataset.py
+for mode in "train" "val"
+do
+	folder=$(pwd)"/original_data/"$mode"/"
+	for file in $(ls $folder)
+	do
+		srun -n 1 python3 create_dataset.py --result_dir $(pwd)"/dataset_QSM/"$mode"/" --filename $folder$file
+	done
+done
+
 conda deactivate
